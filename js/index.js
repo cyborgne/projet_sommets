4669a9fd78e69775e661c563e1a1e0a8f1ed8994
Cyborgne <tomassoni.nathan@gmail.com>
2024-11-23 01:09:49 +0100

    Suppression de console.log() utiliser pour déboguer

--- js/index.js
+++ js/index.js
@@ -3,15 +3,12 @@ $(document).ready(function() {
     
     if (!user) {
         // Redirige vers la page de connexion si l'utilisateur n'est pas authentifié
-        console.log('redicrect vers login : ', user)
         window.location.href = 'login.html';
     } else {
         // Redirection basée sur le rôle de l'utilisateur, si nécessaire
         if (user.userRole == 'editor') {
-            console.log('redicrect vers edit : ', user)
             window.location.href = 'edit.html';
         } else {
-            console.log('redicrect vers view : ', user)
             window.location.href = 'view.html';
         } 
     }
