71de80b2e95aca788e75d8f8cda09539afaf6c9a
Constantl57 <constant.lafosse@gmail.com>
2024-11-25 23:49:42 +0100

    revue des parametres de redirection pour éviter les bugs

--- js/index.js
+++ js/index.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
     var user = JSON.parse(sessionStorage.getItem('user'));
-    
-    if (!user) {
-        // Redirige vers la page de connexion si l'utilisateur n'est pas authentifié
+    var currentPage = window.location.pathname.split('/').pop(); // Obtenir le nom de la page actuelle
+
+    if (!user && currentPage !== 'login.html') {
+        // Redirige uniquement si l'utilisateur n'est pas authentifié ET n'est pas déjà sur login.html
         window.location.href = 'login.html';
-    } else {
+    } else if (user) {
         // Redirection basée sur le rôle de l'utilisateur, si nécessaire
-        if (user.userRole == 'editor') {
+        if (user.userRole == 'editor' && currentPage !== 'edit.html') {
             window.location.href = 'edit.html';
-        } else {
+        } else if (user.userRole != 'editor' && currentPage !== 'view.html') {
             window.location.href = 'view.html';
-        } 
+        }
     }
 });
+
+
